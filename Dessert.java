d564bc48c811451c5e2e020e11375485c5afb411
Noemi <noemibarone00@gmail.com>
2023-04-28 19:48:23 +0200

    Dessert

---
+++ Dessert.java
@@ -0,0 +1,29 @@
+public class Dessert {
+    private String piatto;
+    private double prezzo;
+
+    public Dessert(String piatto, double prezzo) {
+        this.piatto = piatto;
+        this.prezzo = prezzo;
+    }
+
+    public String getPiatto() {
+        return piatto;
+    }
+
+    public void setPiatto(String piatto) {
+        this.piatto = piatto;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void printDessert(){
+        System.out.println("- " + piatto + "       € " + prezzo);
+    }
+}
