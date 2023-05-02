328d981a94a9342f5e12b8167fff40f6ef4df04a
Lorenzo2211 <lorenzosantonocito01@gmail.com>
2023-05-02 15:48:30 +0200

    Secondi Piatti

---
+++ SecondiPiatti.java
@@ -0,0 +1,29 @@
+public class SecondiPiatti {
+    private String piatto;
+    private double prezzo;
+
+    public SecondiPiatti(String piatto, double prezzo) {
+        this.piatto = piatto;
+        this.prezzo = prezzo;
+    }
+
+    public String getPiatto() {
+        return piatto;
+    }
+
+    public void setPiatto(String piatto) {
+        this.piatto = piatto;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void printDessert(){
+        System.out.println("- " + piatto + "       € " + prezzo);
+    }
+}
