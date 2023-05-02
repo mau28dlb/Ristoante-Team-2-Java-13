09825d33e84474cd72d92fc245f72b7ec9ab3913
Mauro Isidoro Diliberto <mau.dilib@gmail.com>
2023-05-02 15:55:50 +0200

    aggiunto classe bevande

---
+++ Bevande.java
@@ -0,0 +1,31 @@
+public class Bevande {
+
+    private String descrizione;
+    private double prezzo;
+
+    public Bevande(String descrizione, double prezzo) {
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void printBevande() {
+
+        System.out.println("- " + descrizione + "       € " + prezzo);
+    }
+}
